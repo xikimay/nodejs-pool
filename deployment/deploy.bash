9876eac40fc2fa6319131a5c1f7e022572d4d922
root <root@ns3041366.ip-164-132-201.eu>
2017-12-11 22:17:49 +0000

    update deployment

--- deployment/deploy.bash
+++ deployment/deploy.bash
@@ -28,6 +28,13 @@ sudo git clone https://github.com/UnitedBitcoin/UnitedBitcoin.git
 cd UnitedBitcoin
 sudo git checkout
 curl https://raw.githubusercontent.com/xikimay/nodejs-pool/master/deployment/ub_daemon.patch | sudo git apply -v
+sudo apt-get install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils python3
+
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:bitcoin/bitcoin
+sudo apt-get update
+sudo apt-get install libdb4.8-dev libdb4.8++-dev
+
 sudo ./autogen.sh
 sudo ./configure
 sudo make
