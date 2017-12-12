df0f2a7696840968e6dea028a0114b8a77da7cd2
Cheun Marec <cheunmarec@Cheuns-MacBook-Pro.local>
2017-12-12 16:06:46 +0100

    update

--- deployment/upgrade_ub.bash
+++ deployment/upgrade_ub.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "This assumes that you have a standard nodejs-pool install, and will patch and update it to the latest stable builds of Monero."
+echo "This assumes that you have a standard nodejs-pool install, and will patch and update it to the latest stable builds of UnitedBitcoin."
 echo "Continuing install, this will prompt you for your password if you didn't enable passwordless sudo.  Please do not run me as root!"
 cd /usr/local/src/UnitedBitcoin
 sudo git checkout .
@@ -9,4 +9,4 @@ sudo git checkout v1.0.2
 curl -L https://raw.githubusercontent.com/xikimay/nodejs-pool/master/deployment/ub_daemon.patch | sudo git apply -v
 sudo rm -rf build
 sudo make -j$(nproc)
-echo "Done building the new Monero daemon!  Please go ahead and reboot monero with: sudo systemctl restart monero as soon as the pool source is updated!"
+echo "Done building the new UnitedBitcoin daemon!  Please go ahead and reboot monero with: sudo systemctl restart ub as soon as the pool source is updated!"
